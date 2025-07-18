dc52c1e3235862670062bf47e2200a68bfbef481
Mohd Anas Siddique <mohdanas897@gmail.com>
2025-07-18 14:36:51 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,10 @@
 variable "custom_port" {
-  description = "Custom port to allow inbound traffic"
+  description = "Custom port to allow inbound traffic on"
   type        = number
   default     = 8080
+}
+
+variable "key_name" {
+  description = "Name of the AWS EC2 key pair"
+  type        = string
 }
